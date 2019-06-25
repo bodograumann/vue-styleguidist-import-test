a26cf8ea7b6d07faa42c2e9703be3aad668c0bcb
Bodo Graumann <mail@bodograumann.de>
2019-06-25 14:20:43 +0200

    Add failing example

--- src/components/AppButton.vue
+++ src/components/AppButton.vue
@@ -59,25 +59,15 @@ This button is amazing, use it responsibly.
 
 ## Examples
 
-Orange button:
-
-```jsx
-<app-button color="orange">Push Me</app-button>
+```vue
+<template>
+  <app-button color="orange">Push Me</app-button>
+</template>
+<script>
+import examples1 from "./examples.js";
+import examples2 from "../examples.js";
+export default {};
+</script>
 ```
 
-Ugly button with pink font and blue background:
-
-```jsx
-<app-button color="pink" background="blue">
-  Ugly button
-</app-button>
-```
-
-Button containing custom tags:
-
-```jsx
-<app-button>
-  Text with <b>bold</b>
-</app-button>
-```
 </docs>
